8c0a527a3aea5d9880189c42081eac442484b5cd
zork0077 <159931049+zork0077@users.noreply.github.com>
2025-05-15 15:28:35 +0300

    Delete currency-crypto-monitor directory

--- currency-crypto-monitor/src/main/java/com/example/currencycryptomonitor/repository/CryptoPriceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.currencycryptomonitor.repository;
-
-import com.example.currencycryptomonitor.model.CryptoPrice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CryptoPriceRepository extends JpaRepository<CryptoPrice, Long> {}
